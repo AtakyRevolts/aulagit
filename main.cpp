15bf0507f2c4d9a9c3c18a6d0c9b7dd8a6ac125b
AtakyRevolts <atakyamanik@gmail.com>
2022-08-18 08:51:18 -0300

    Adicionado fatorial e .gitignore

--- main.cpp
+++ main.cpp
@@ -1,7 +1,16 @@
 #include <stdio.h>
 
+int fatorial(int n);
+
 int main(){
-	printf("Hello Git!\n");
+	printf("Fatorial(5) = %i\n",fatorial(5));
 	return 0;
 }
 
+int fatorial(int n){
+	if(n == 0){
+		return 1;
+	} else {
+		return n*fatorial(n-1);
+	}
+}
